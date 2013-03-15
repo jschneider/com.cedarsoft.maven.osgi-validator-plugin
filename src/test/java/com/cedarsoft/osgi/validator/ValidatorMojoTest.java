3862ca2e56ae0d04cb4abab03d946f8e0922ea5c
Johannes Schneider <js@cedarsoft.com>
2013-03-15 15:48:06 +0100

    switching to package based mode

--- src/test/java/com/cedarsoft/osgi/validator/ValidatorMojoTest.java
+++ src/test/java/com/cedarsoft/osgi/validator/ValidatorMojoTest.java
@@ -27,45 +27,59 @@ public class ValidatorMojoTest extends AbstractMojoTestCase {
     loggerManager.setThreshold(Logger.LEVEL_DEBUG);
   }
 
-  public void testPartsOsgi() throws Exception {
-    Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "osgi-maven-plugin", Collections.<String>emptySet() );
-    assertThat( allowedPrefixes ).hasSize( 2 );
-    assertThat( allowedPrefixes ).contains( "com/cedarsoft/osgi/maven/plugin" );
-    assertThat( allowedPrefixes ).contains( "com/cedarsoft/osgi" );
-  }
-
-  public void testParts() throws Exception {
-    Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myGroup", Collections.<String>emptySet() );
-    assertThat( allowedPrefixes ).hasSize( 1 );
-    assertThat( allowedPrefixes ).contains( "com/cedarsoft/myGroup" );
-  }
-
-  public void testParts2() throws Exception {
-    Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myGroup", ImmutableSet.of( "cedarsoft" ) );
-    assertThat( allowedPrefixes ).hasSize( 2 );
-    assertThat( allowedPrefixes )
-      .contains( "com/cedarsoft/myGroup" )
-      .contains( "com/myGroup" )
-    ;
-  }
-
-  public void testSkip2() throws Exception {
-    Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myart", ImmutableSet.of( "myart" ) );
-    assertThat( allowedPrefixes ).hasSize( 2 );
-    assertThat( allowedPrefixes )
-      .contains( "com/cedarsoft/myart" )
-      .contains( "com/cedarsoft" )
-    ;
-  }
-
-  public void testMaven() throws Exception {
-    Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft-ear", "test-asdf-maven-plugin", ImmutableSet.of( "myart" ) );
-    assertThat( allowedPrefixes ).hasSize( 2 );
-    assertThat( allowedPrefixes )
-      .contains( "com/cedarsoft/ear/test/asdf" )
-      .contains( "com/cedarsoft/ear/test/asdf/maven/plugin" )
-    ;
-  }
+  //public void testMultiSkip() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft.commons", "xml-common", ImmutableSet.of( "commons", "common" ) );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/commons/xml/common" );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/xml" );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //}
+  //
+  //public void testIssue1() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft.commons", "xml-commons", ImmutableSet.of( "commons" ) );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/commons/xml/commons" );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/xml" );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //}
+  //
+  //public void testPartsOsgi() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "osgi-maven-plugin", Collections.<String>emptySet() );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/osgi/maven/plugin" );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/osgi" );
+  //}
+  //
+  //public void testParts() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myGroup", Collections.<String>emptySet() );
+  //  assertThat( allowedPrefixes ).hasSize( 1 );
+  //  assertThat( allowedPrefixes ).contains( "com/cedarsoft/myGroup" );
+  //}
+  //
+  //public void testParts2() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myGroup", ImmutableSet.of( "cedarsoft" ) );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //  assertThat( allowedPrefixes )
+  //    .contains( "com/cedarsoft/myGroup" )
+  //    .contains( "com/myGroup" )
+  //  ;
+  //}
+  //
+  //public void testSkip2() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft", "myart", ImmutableSet.of( "myart" ) );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //  assertThat( allowedPrefixes )
+  //    .contains( "com/cedarsoft/myart" )
+  //    .contains( "com/cedarsoft" )
+  //  ;
+  //}
+  //
+  //public void testMaven() throws Exception {
+  //  Set<String> allowedPrefixes = ValidatorMojo.createAllowedPrefixes( "com.cedarsoft-ear", "test-asdf-maven-plugin", ImmutableSet.of( "myart" ) );
+  //  assertThat( allowedPrefixes ).hasSize( 2 );
+  //  assertThat( allowedPrefixes )
+  //    .contains( "com/cedarsoft/ear/test/asdf" )
+  //    .contains( "com/cedarsoft/ear/test/asdf/maven/plugin" )
+  //  ;
+  //}
 
   public void testBasic() throws Exception {
     ValidatorMojo mojo = createMojo("basic.xml");
@@ -109,24 +123,24 @@ public class ValidatorMojoTest extends AbstractMojoTestCase {
     return mojo;
   }
 
-  public void testPackagePath() throws Exception {
-    assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft", "test")).isEqualTo("com/cedarsoft/test");
-    assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft", "test-asdf")).isEqualTo("com/cedarsoft/test/asdf");
-    assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft-ear", "test-asdf")).isEqualTo("com/cedarsoft/ear/test/asdf");
-
-    assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft-ear", "test-asdf-maven-plugin")).isEqualTo("com/cedarsoft/ear/test/asdf");
-  }
-
-  public void testPossibleIds() throws Exception {
-    assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.commons.history")).contains("com.cedarsoft.history");
-    assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.commons.history")).contains("com.cedarsoft.commons.history");
-  }
-
-  public void testMorePossibleIds() throws Exception {
-    assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.domain.history", ImmutableSet.of( "domain" ))).hasSize( 2 )
-      .contains("com.cedarsoft.history")
-      .contains("com.cedarsoft.domain.history")
-    ;
-    assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.domain-asdf.history", ImmutableSet.of("domain"))).contains("com.cedarsoft.asdf.history");
-  }
+  //public void testPackagePath() throws Exception {
+  //  assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft", "test")).isEqualTo("com/cedarsoft/test");
+  //  assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft", "test-asdf")).isEqualTo("com/cedarsoft/test/asdf");
+  //  assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft-ear", "test-asdf")).isEqualTo("com/cedarsoft/ear/test/asdf");
+  //
+  //  assertThat(ValidatorMojo.createAllowedPrefix("com.cedarsoft-ear", "test-asdf-maven-plugin")).isEqualTo("com/cedarsoft/ear/test/asdf");
+  //}
+  //
+  //public void testPossibleIds() throws Exception {
+  //  assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.commons.history")).contains("com.cedarsoft.history");
+  //  assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.commons.history")).contains("com.cedarsoft.commons.history");
+  //}
+  //
+  //public void testMorePossibleIds() throws Exception {
+  //  assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.domain.history", ImmutableSet.of( "domain" ))).hasSize( 2 )
+  //    .contains("com.cedarsoft.history")
+  //    .contains("com.cedarsoft.domain.history")
+  //  ;
+  //  assertThat(ValidatorMojo.createPossibleIds("com.cedarsoft.domain-asdf.history", ImmutableSet.of("domain"))).contains("com.cedarsoft.asdf.history");
+  //}
 }
